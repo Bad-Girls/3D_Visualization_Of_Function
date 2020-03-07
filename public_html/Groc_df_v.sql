205a1d8870c5f0b412f773428bbccd1be47c559e
Bad-Girls <109237262+Bad-Girls@users.noreply.github.com>
2020-03-07 09:56:48 +0000

    update hashes

--- public_html/Groc_df_v.sql
+++ public_html/Groc_df_v.sql
@@ -14,4 +14,12 @@ SELECT
   `stores`.`cluster` AS `s_cluster`,
   `oil`.`dcoilwtico` AS `o_dcoilwtico`,
   `transactions`.`transactions` AS `t_transactions`,
-   CASE WHEN h1.description IS NOT NULL THEN 1 ELSE 0 END A
+   CASE WHEN h1.description IS NOT NULL THEN 1 ELSE 0 END AS h_is_holiday,
+   h1.type AS h_type,
+   h1.locale AS h_locale,
+   h1.locale_name AS h_locale_name,
+   h1.description AS h_description,
+   h1.transferred AS h_transferred
+FROM
+  `train_sample1` JOIN `items` ON`items`.`item_nbr` = `train_sample1`.`item_nbr`
+  JOIN `stores` ON `stores`.`store_nbr` = `train_sample1`.`sto
