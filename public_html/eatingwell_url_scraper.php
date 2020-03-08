ff250efbe614f71c293a6a0a6b1bfa69310616e9
Bad-Girls <109237262+Bad-Girls@users.noreply.github.com>
2020-03-08 06:27:28 +0000

    Update submodules: plugins (new commits)

--- public_html/eatingwell_url_scraper.php
+++ public_html/eatingwell_url_scraper.php
@@ -9,4 +9,15 @@ use Goutte\Client;
 $pdo = DbConn::getPdo();
 
 $client = new Client();
-$gzl
+$gzl = new GuzzleHttp\Client( array(
+  'curl' => array(
+    CURLOPT_SSL_VERIFYPEER => false,
+  ),
+) );
+$client->setClient($gzl);
+
+for( $i=200; $i< 340; $i++ ){
+  $statement = $pdo->prepare("SELECT * FROM EatingWellRecipe WHERE PageNumber=?");
+  $page_id = $i;
+  $statement->execute([$page_id]);
+  $existing = $statem
