ff6f4984d9618aef11645fa85d9e4d5688c9d98f
Bad-Girls <109237262+Bad-Girls@users.noreply.github.com>
2020-03-09 14:28:59 +0000

    wibble

--- public_html/includes/DbConn.php
+++ public_html/includes/DbConn.php
@@ -12,4 +12,13 @@ class DbConn {
   public static function getPdo(){
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $dsn = "mysql:host=".static::$host.";dbname=".static::$db.";charset=".static::$charset;
+    if( static::$pdo == NULL ){
+      static::$pdo = new PDO($dsn, static::$user, static::$pass, $opt);
+    }
+    return static::$pdo;
+  }
+}
