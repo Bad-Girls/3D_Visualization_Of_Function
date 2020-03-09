f5efb8f8093831f865429b1f178ab62a6609e43e
Bad-Girls <109237262+Bad-Girls@users.noreply.github.com>
2020-03-09 11:04:09 +0000

    More optimization

--- public_html/includes/DbConn.php
+++ public_html/includes/DbConn.php
@@ -1,4 +1,15 @@
 <?php
 
 class DbConn {
-  public static $pdo = NULL
+  public static $pdo = NULL;
+  public static $host = '127.0.0.1';
+  public static $db = 'recipesub';
+  public static $user = 'recipesub';
+  public static $pass = 'Latter!Padua!Valium1';
+  public static $charset = 'utf8mb4';
+
+
+  public static function getPdo(){
+    $opt = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR
