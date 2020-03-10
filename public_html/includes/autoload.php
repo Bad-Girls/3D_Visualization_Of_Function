d421b2950804735a41ff537d2b38ac777a33ea4d
Bad-Girls <109237262+Bad-Girls@users.noreply.github.com>
2020-03-10 09:35:40 +0000

    added placeholder

--- public_html/includes/autoload.php
+++ public_html/includes/autoload.php
@@ -1,3 +1,13 @@
 <?php
 
-ini_set('displ
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once( $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php' );
+
+spl_autoload_register(function ($class_name) {
+  $fname = "{$_SERVER['DOCUMENT_ROOT']}/includes/$class_name.php";
+  if( file_exists($fname) )
+    require_once $fname;
+});
