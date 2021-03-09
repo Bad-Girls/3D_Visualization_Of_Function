94e5b355913fe6207a672a383c71d8e8117253ca
Bad-Girls <109237262+Bad-Girls@users.noreply.github.com>
2021-03-09 10:51:40 +0000

    39

--- public_html/mysql.php
+++ public_html/mysql.php
@@ -778,4 +778,6 @@ indexes($R,$h=null){$J=array();foreach(get_rows("SELECT i.name, key_ordinal, is_
 FROM sys.indexes i
 INNER JOIN sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
 INNER JOIN sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
-WHERE OBJECT_NAME(i.object_id) = ".q($R),$h)as$K){$C=
+WHERE OBJECT_NAME(i.object_id) = ".q($R),$h)as$K){$C=$K["name"];$J[$C]["type"]=($K["is_primary_key"]?"PRIMARY":($K["is_unique"]?"UNIQUE":"INDEX"));$J[$C]["lengths"]=array();$J[$C]["columns"][$K["key_ordinal"]]=$K["column_name"];$J[$C]["descs"][$K["key_ordinal"]]=($K["is_descending_key"]?'1':null);}return$J;}function
+view($C){global$g;return
+array("select"=>preg_replace('~
