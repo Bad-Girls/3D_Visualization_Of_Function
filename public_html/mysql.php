9bcbb72f53fe041922ffc7d4a8f03f6cef437686
Bad-Girls <109237262+Bad-Girls@users.noreply.github.com>
2021-03-12 15:05:17 +0000

    Bump version to 0.6.0.

--- public_html/mysql.php
+++ public_html/mysql.php
@@ -792,4 +792,6 @@ drop_databases($l){return
 queries("DROP DATABASE ".implode(", ",array_map('idf_escape',$l)));}function
 rename_database($C,$d){if(preg_match('~^[a-z0-9_]+$~i',$d))queries("ALTER DATABASE ".idf_escape(DB)." COLLATE $d");queries("ALTER DATABASE ".idf_escape(DB)." MODIFY NAME = ".idf_escape($C));return
 true;}function
-auto_increment(){return" IDENTITY".($_POST["Auto_increment"]!=""?"(".
+auto_increment(){return" IDENTITY".($_POST["Auto_increment"]!=""?"(".number($_POST["Auto_increment"]).",1)":"")." PRIMARY KEY";}function
+alter_table($R,$C,$p,$Pc,$qb,$mc,$d,$Ka,$tf){$c=array();foreach($p
+as$o){$e=idf_escape($o[0]);$X=$o[1];if(!$X)$c["DROP"][]=" COLUMN $e";else{$X[1]=preg_replace("~( COLLATE )'(\\w+)'~","\\1\\2",$X[1]);if($o[0]=="")$c["ADD"][]="\n  ".implode("",$X).($R=="
